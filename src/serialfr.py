c109078d55f0dc7850e299ed592e48d8ddf6db93
Matthew Padilla <matthewpadilla02@gmail.com>
2024-11-03 11:26:26 -0500

    stuff

--- src/serialfr.py
+++ src/serialfr.py
@@ -15,6 +15,5 @@ class serialfr:
         if self.serialInst.in_waiting:
             packet = self.serialInst.readline()
             lst = packet.decode('utf').split(' ')
-            print(lst[0])
             return float(lst[0]), float(lst[1]), float(lst[2]), float(lst[3])
         return 0,0,0,0
